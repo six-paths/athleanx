32fbda98e561ef4c06cac9f00ae73819c2666ef8
sione-jack-ohara <jack.ohara@sione.co.uk>
2020-05-06 14:06:37 +0100

    Updates

---
+++ precache-manifest.ddccc703490538c4ad2a621dec16b6bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4be043059400f66e6900c0665de777f",
+    "url": "/athleanx/index.html"
+  },
+  {
+    "revision": "1a58267a54d2f1a1c860",
+    "url": "/athleanx/static/css/main.45800387.chunk.css"
+  },
+  {
+    "revision": "b2ddb579f9037dd6c658",
+    "url": "/athleanx/static/js/2.bff6a107.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/athleanx/static/js/2.bff6a107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a58267a54d2f1a1c860",
+    "url": "/athleanx/static/js/main.8018df8b.chunk.js"
+  },
+  {
+    "revision": "50a1a72ba12b2f300d29",
+    "url": "/athleanx/static/js/runtime-main.cbce87f2.js"
+  }
+]);
